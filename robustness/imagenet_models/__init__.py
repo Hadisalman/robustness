3346f1267d50ed084778c91307d99f903ce56dd7
Hadi Salman <hadicsalman@gmail.com>
2020-02-21 05:13:43 +0000

    clean code. add new exp:AT sweep over archs

--- robustness/imagenet_models/__init__.py
+++ robustness/imagenet_models/__init__.py
@@ -1,6 +1,5 @@
-from .resnet import *
+# from .resnet import *
+from .resnet_official import * 
 from .vgg import *
-# from .wide_resnet import wide_resnet50 # Buggy
-from .wide_resnet_official import wide_resnet50_2
 from .leaky_resnet import *
 from .alexnet import *
